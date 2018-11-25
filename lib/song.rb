117b08a4e1f8c48d7ab285edef53a300ca189bcf
Robin Kim <rkim1214@gmail.com>
2018-11-25 19:49:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,12 @@ class Song
   
   @@all = []
   
-  attr_accessor :name
+  attr_accessor :name, :artist, :genre
   
   def initialize(title, artist, genre)
     @name = title
+    @artist = artist
+    @genre = genre
   end 
   
 end 
