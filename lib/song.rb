f73ac19485e3713304155712c2ed40d3b91c1ec8
Robin Kim <rkim1214@gmail.com>
2018-11-25 19:52:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   
   @@count = 0
   @@artists = []
+  @@genres = []
   
   attr_accessor :name, :artist, :genre
   
@@ -11,6 +12,7 @@ class Song
     @genre = genre
     @@count += 1
     @@artists << @artist
+    @@genres << @genre
   end 
   
 end 
